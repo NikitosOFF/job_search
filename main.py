9bac27c196cc505e9c259405947c67eabb34b23c
n.druzhinin <n.druzhinin@bestdoctor.ru>
2019-11-06 22:55:44 +0300

    v_1.1

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def predict_rub_salary(job_title):
     parametres = {'text': 'программист {}'.format(job_title), 'area': '1', 'period': '30', 'only_with_salary': 'true'}
     response = requests.get(url, params=parametres)
     salary_of_various_vacancies = []
+    salary_statistics = {}
     for vacancy in response.json()['items']:
         if vacancy['salary']['currency'] != 'RUR':
             break
@@ -15,23 +16,14 @@ def predict_rub_salary(job_title):
             salary_of_various_vacancies.append(vacancy['salary']['from'] * 1.2)
         elif vacancy['salary']['from'] is not None and vacancy['salary']['to'] is not None:
             salary_of_various_vacancies.append((vacancy['salary']['to'] + vacancy['salary']['from']) / 2)
-    print(salary_of_various_vacancies)
-    print(len(salary_of_various_vacancies))
-    print(response.json()['found'])
+    salary_statistics['vacancies_found'] = response.json()['found']
+    salary_statistics['vacancies_processed'] = len(salary_of_various_vacancies)
+    salary_statistics['average_salary'] = (int(sum(salary_of_various_vacancies) / len(salary_of_various_vacancies)))
+    return salary_statistics
 
 
-popular_programming_languages = ['Python', 'Java', 'Javascript', 'Ruby', 'PHP', 'Swift', 'Go', 'Shell']
+popular_programming_languages = ['Python', 'Java', 'Javascript', 'Ruby', 'Objective-C', 'Swift', 'Go', 'Shell']
+language_salary_statistics = {}
 for programming_language in popular_programming_languages:
-    predict_rub_salary(programming_language)
-# number_of_programming_language_vacancies={}
-# programming_languages = ['Python', 'Java', 'Javascript', 'Ruby', 'PHP', 'C++', 'C#', 'C']
-# lang = ['Python']
-# for lang in programming_languages:
-# url = 'https://api.hh.ru/vacancies/'
-# parametres = {'text': 'программист {}'.format(lang), 'area': '1', 'period' : '30', 'only_with_salary': 'true'}
-# response = requests.get(url, params=parametres)
-# response.raise_for_status()
-# number_of_programming_language_vacancies[lang]=response.json()['found']
-# print([vacancy['salary'] for vacancy in response.json()['items']])
-# print(number_of_programming_language_vacancies)
-# predict_rub_salary(input())
+    language_salary_statistics['{}'.format(programming_language)] = predict_rub_salary(programming_language)
+print(language_salary_statistics)
